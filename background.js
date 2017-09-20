c928aa02d002dda9c2339e63b5d1a682187946c5
Andreas Bielawski <brawl98@outlook.com>
2017-09-20 11:23:42 +0200

    v3.1.1: * Fix TinEye search (resolves #18)

--- background.js
+++ background.js
@@ -13,7 +13,7 @@ var Provider = class Provider {
 const defaultProviders = [
 	new Provider('Google', '../icons/google.png', 'https://www.google.com/searchbyimage?image_url=%s', true),
 	new Provider('IQDB', '../icons/iqdb.png', 'https://iqdb.org/?url=%s'),
-	new Provider('TinEye', '../icons/tineye.png', 'https://www.tineye.com/parse?url=%s'),
+	new Provider('TinEye', '../icons/tineye.png', 'https://www.tineye.com/search?url=%s'),
 	new Provider('Bing', '../icons/bing.png', 'https://www.bing.com/images/searchbyimage?FORM=IRSBIQ&cbir=sbi&imgurl=%s'),
 	new Provider('Yandex', '../icons/yandex.png', 'https://yandex.com/images/search?url=%s&rpt=imageview'),
 	new Provider('Яндекс', '../icons/yandexru.png', 'https://yandex.ru/images/search?url=%s&rpt=imageview'),
