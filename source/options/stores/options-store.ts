69fa2c01d840fe9024974e154ac3e9da2c88ca39
Andreas Bielawski <brawl@nyanya.de>
2024-08-14 22:47:18 +0200

    Clean up options store a bit

--- source/options/stores/options-store.ts
+++ source/options/stores/options-store.ts
@@ -1,14 +1,11 @@
 import { writable } from 'svelte/store';
 import { defaultOptions, getOptions, newProvider } from '../../storage';
-import type { Options, StorageProvider } from '../../types';
+import type { Options } from '../../types';
 import { arraymove, getLowerIndex, getUpperIndex } from '../../utils.js';
-
-const cloneProviders = () =>
-  defaultOptions.storageProviders.map((p) => ({ ...p }));
 
 const createStore = () => {
   const { subscribe, set, update } = writable<Options>(
-    JSON.parse(JSON.stringify(defaultOptions)),
+    structuredClone(defaultOptions),
   );
 
   return {
@@ -22,43 +19,36 @@ const createStore = () => {
     removeProvider: (indexToRemove: number) =>
       update((previous) => ({
         ...previous,
-        storageProviders: [
-          ...previous.storageProviders.filter(
-            (_provider: StorageProvider, providerIndex: number) =>
-              providerIndex !== indexToRemove,
-          ),
-        ],
+        storageProviders: previous.storageProviders.filter(
+          (_, i) => i !== indexToRemove,
+        ),
       })),
     resetProviders: () =>
       update((previous) => ({
         ...previous,
-        storageProviders: cloneProviders(),
+        storageProviders: defaultOptions.storageProviders.map((p) => ({
+          ...p,
+        })),
       })),
-    moveProviderUp: (providerIndex: number) => {
+    moveProviderUp: (providerIndex: number) =>
       update((previous) => ({
         ...previous,
-        storageProviders: [
-          ...arraymove(
-            previous.storageProviders,
-            providerIndex,
-            getUpperIndex(previous.storageProviders.length, providerIndex),
-          ),
-        ],
-      }));
-    },
-    moveProviderDown: (providerIndex: number) => {
+        storageProviders: arraymove(
+          previous.storageProviders,
+          providerIndex,
+          getUpperIndex(previous.storageProviders.length, providerIndex),
+        ),
+      })),
+    moveProviderDown: (providerIndex: number) =>
       update((previous) => ({
         ...previous,
-        storageProviders: [
-          ...arraymove(
-            previous.storageProviders,
-            providerIndex,
-            getLowerIndex(previous.storageProviders.length, providerIndex),
-          ),
-        ],
-      }));
-    },
-    loadFromStorage: () => getOptions().then(set),
+        storageProviders: arraymove(
+          previous.storageProviders,
+          providerIndex,
+          getLowerIndex(previous.storageProviders.length, providerIndex),
+        ),
+      })),
+    loadFromStorage: async () => set(await getOptions()),
   };
 };
 
